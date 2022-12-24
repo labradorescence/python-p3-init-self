071e4d4e0fd362e0f6697232c43728cf91ddff5e
labradorescence <orangenewyorkcreativeagency@gmail.com>
2022-12-23 20:46:21 -0500

    practice

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Dog:
+
+    def __init__(self, name, breed = "Mutt"): #init method
+        self.name = name #attribute1
+        self.breed = breed #attribute2
     pass
+
+pass
+
+sadie = Dog("Sadie")
+print(sadie.name)
